2415a5cab20cd8607ff3673bc51ff5f50fd17035
Terry MARROT <terry.marrot@etu.univ-grenoble-alpes.fr>
2023-12-06 14:19:35 +0100

    correction checkstatsnotok

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { Hero } from '../data/hero';
 import { HeroService } from '../service/hero.service';
 import { MessageService } from '../service/message.service';
 import { Router } from '@angular/router';
-import {Subscription} from "rxjs";
+import {firstValueFrom, Subscription} from "rxjs";
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -14,20 +14,24 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
   subscriptionGetHeroes?: Subscription;
-
+  FilterValue: number | undefined;
   heroes: Hero[] = [];
-
+  filteredHeroes: Hero[] = []; // Ajout de la liste filtrée
   constructor(
     private heroService: HeroService,
     private router: Router) { }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.getHeroes();
+    this.filteredHeroes =this.heroes;
+    console.log(this.heroes);
   }
 
   getHeroes(): void {
-    this.subscriptionGetHeroes =
-        this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+      this.subscriptionGetHeroes = this.heroService.getHeroes().subscribe(heroes => {
+        this.heroes = heroes;
+        this.filteredHeroes = [...this.heroes];
+      });
   }
 
   createHero(): void {
@@ -36,6 +40,43 @@ export class HeroesComponent implements OnInit {
         this.router.navigate(['heroDetail/' + h.id]);
       })
 
+  }
+  resetFilter(): void{
+    this.filteredHeroes = this.heroes;
+  }
+  // Fonction pour filtrer les héros par type
+  filterByAttaque(): void {
+    if(this.FilterValue) {
+      this.filteredHeroes = this.heroes.filter(hero => hero.attaque === this.FilterValue);
+  }}
+  filterByPV(): void {
+    if(this.FilterValue) {
+      this.filteredHeroes = this.heroes.filter(hero => hero.PV === this.FilterValue);
+    }  }
+  filterByDegats(): void {
+    if(this.FilterValue) {
+      this.filteredHeroes = this.heroes.filter(hero => hero.degats === this.FilterValue);
+    }}
+  filterByEsquive(): void {
+    if(this.FilterValue) {
+      this.filteredHeroes = this.heroes.filter(hero => hero.esquive === this.FilterValue);
+    }  }
+  // Fonction pour trier les héros par attribut
+  sortHeroesByAttack(): void {
+    console.log('Avant le tri par attaque :', this.filteredHeroes.map(hero => hero.attaque));
+    this.filteredHeroes.sort((a, b) => (a.attaque > b.attaque) ? 1 : -1);
+    console.log('Après le tri par attaque :', this.filteredHeroes.map(hero => hero.attaque));
+  }
+
+  sortHeroesByDegats(): void {
+    this.filteredHeroes.sort((a, b) => (a.degats > b.degats) ? 1 : -1);
+  }
+  sortHeroesByPV(): void {
+    this.filteredHeroes.sort((a, b) => (a.PV > b.PV) ? 1 : -1);
+  }
+
+  sortHeroesByEsquive(): void {
+    this.filteredHeroes.sort((a, b) => (a.esquive > b.esquive) ? 1 : -1);
   }
   ngOnDestroy(): void {
 
